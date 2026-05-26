71a82f4d83b207e57f8518e483e8d0287fbb9c5d
Amber Goulding <gouldinga1@cardiff.ac.uk>
2026-05-26 18:16:32 +0100

    add minimal uci support

---
+++ src/uci.cpp
@@ -0,0 +1,59 @@
+/*
+  Surveyor - A UCI chess engine.
+  Copyright (C) 2026 Amber Goulding
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "uci.hpp"
+
+#include <iostream>
+#include <print>
+#include <sstream>
+
+namespace surveyor {
+
+auto uci::loop() -> void {
+  std::string line, cmd;
+
+  while (std::getline(std::cin, line)) {
+    std::istringstream ss(line);
+    ss >> cmd;
+
+    const std::optional<std::unique_ptr<uci_error>> err = dispatch_command(cmd, ss);
+
+    if (err) {
+      std::println("Error: {}", **err);
+    }
+  }
+}
+
+auto uci::dispatch_command(std::string_view command, std::istringstream& arguments)
+  -> std::optional<std::unique_ptr<uci_error>> {
+  if (command == "uci") {
+    return execute_uci(arguments);
+  }
+
+  return std::make_unique<uci_error_bad_cmd>(command);
+}
+
+auto uci::execute_uci(std::istringstream& arguments) -> std::optional<std::unique_ptr<uci_error>> {
+  (void)arguments;
+  std::println("id name Surveyor");
+  std::println("id author Amber Goulding");
+  std::println("uciok");
+  return std::nullopt;
+}
+
+}  // namespace surveyor
---
+++ src/uci.hpp
@@ -0,0 +1,62 @@
+/*
+  Surveyor - A UCI chess engine.
+  Copyright (C) 2026 Amber Goulding
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#include <format>
+#include <memory>
+#include <string_view>
+
+namespace surveyor {
+
+struct uci_error {
+  virtual ~uci_error() = default;
+
+  [[nodiscard]] virtual auto msg() const -> std::string = 0;
+};
+
+struct uci_error_bad_cmd : uci_error {
+  uci_error_bad_cmd(std::string_view cmd)
+      : cmd(cmd) {
+  }
+
+  [[nodiscard]] auto msg() const -> std::string override {
+    return std::format("bad command \"{}\".", cmd);
+  }
+
+  std::string cmd;
+};
+
+class uci {
+public:
+  auto loop() -> void;
+
+private:
+  auto dispatch_command(std::string_view command, std::istringstream& arguments)
+    -> std::optional<std::unique_ptr<uci_error>>;
+
+  auto execute_uci(std::istringstream& arguments) -> std::optional<std::unique_ptr<uci_error>>;
+};
+
+}  // namespace surveyor
+
+template<>
+struct std::formatter<surveyor::uci_error> : std::formatter<std::string> {
+  auto format(const surveyor::uci_error& err, std::format_context& ctx) const {
+    return std::formatter<std::string>::format(err.msg(), ctx);
+  }
+};
