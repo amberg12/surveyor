ea438073f042b3353725627e7e865b16462bac16
amberg12 <amber.g.dev@protonmail.com>
2026-06-08 16:34:52 +0100

    add movepicker to perft (#15)
    54006379 nodes 557611 nps

--- src/move_generation.hpp
+++ src/move_generation.hpp
@@ -21,49 +21,10 @@
 #include "position.hpp"
 #include "util/static_vector.hpp"
 
-#include <print>
-
 namespace surveyor {
 
 using move_list = static_vector<move, 256>;
 
 auto generate_moves(const position& pos) -> move_list;
-
-enum class perft_settings {
-  bulk,
-  standard,
-};
-
-template <perft_settings ps, bool is_root = true>
-constexpr auto perft(const position& pos, i32 depth) -> u64 {
-  if (depth <= 0) {
-    return 1;
-  }
-
-  const move_list ml = generate_moves(pos);
-
-  if (!is_root && ps == perft_settings::bulk && depth == 1) {
-    return ml.size();
-  }
-
-  u64 nodes = 0;
-
-  for (const move mv : ml) {
-    const position child = pos.make_move(mv);
-    const u64 child_nodes = perft<ps, false>(child, depth - 1);
-
-    if constexpr (is_root) {
-      std::println("{}: {}", mv, child_nodes);
-    }
-
-    nodes += child_nodes;
-  }
-
-  if constexpr (is_root) {
-    std::println("nodes: {}", nodes);
-  }
-
-  return nodes;
-}
 
 }  // namespace surveyor
