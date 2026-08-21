a98cdcc8fa7981849b0961c60a45340c1635d3f4
Amber Goulding <gouldinga1@cardiff.ac.uk>
2026-08-21 17:23:38 +0100

    Non-Functional Tweaks
    bench: 3178227

--- surveyor/interface.cpp
+++ surveyor/interface.cpp
@@ -170,6 +170,7 @@ auto interface::uci_uci(tokenizer& toks) -> void {
   std::println("id name surveyor");
   std::println("id author Amber Goulding");
   std::println("option name Hash type spin default 64 min 1 max 1048576");
+  std::println("option name Threads type spin default 1 min 1 max 1");
   std::println("uciok");
 }
 
