22f922623e2454200aaa66cef1395a54848074ed
Amber Goulding <gouldinga1@cardiff.ac.uk>
2026-08-23 18:18:31 +0100

    Add multithreaded datagen
    bench: 440670

--- datagen/__init__.py
+++ datagen/__init__.py
@@ -1,5 +1,6 @@
 __version__ = "0.1.0"
 
 from .cli import main
+from .config import HASH, VERIFICATION_SEARCH_NODES, PRINT_FREQUENCY
+from .data_generation import begin_data_generation
 from .game import GameRunner
-from .config import HASH, VERIFICATION_SEARCH_NODES
--- datagen/cli.py
+++ datagen/cli.py
@@ -1,15 +1,17 @@
 import argparse
+import os
 from datetime import datetime
 from pathlib import Path
-import shutil
 
 from .game import GameRunner
+from .config import PRINT_FREQUENCY
+from .data_generation import begin_data_generation
 
 def main() -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--count", type=int, required=True)
     parser.add_argument("-o", "--out", type=Path)
-
+    parser.add_argument("-j", "--jobs", type=int, default=os.cpu_count())
     args = parser.parse_args()
 
     if args.out is not None:
@@ -20,17 +22,9 @@ def main() -> int:
 
     fout.parent.mkdir(parents=True, exist_ok=True)
 
-    temp = fout.with_suffix(f".1.surveyor-data")
+    n_workers = max(1, min(args.jobs, args.count))
 
-    with temp.open("w") as output:
-        with GameRunner(output) as game_runner:
-            for _ in range(args.count):
-                game_runner.generate_game()
-
-    with fout.open("ab") as output:
-        with temp.open("rb") as input:
-            shutil.copyfileobj(input, output)
-
-    temp.unlink()
+    begin_data_generation(args.count, n_workers, fout)
 
     return 0
+    
