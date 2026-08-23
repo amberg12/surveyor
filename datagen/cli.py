4970dc5ec9344d9a09c061357cd79b50c6771e11
Amber Goulding <gouldinga1@cardiff.ac.uk>
2026-08-23 17:30:36 +0100

    Add datagen game count
    bench: 440670

--- datagen/cli.py
+++ datagen/cli.py
@@ -3,8 +3,15 @@ import argparse
 from .game import GameRunner
 
 def main () -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--count", type=int)
+    args = parser.parse_args()
+
+    game_count = args.count
+
     game_runner = GameRunner(1)
 
-    game_runner.generate_game()
+    for i in range(game_count):
+        game_runner.generate_game()
 
     return 0
