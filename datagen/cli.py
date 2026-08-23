2162f44562c04c25c04832ae8f28b05cd4e33996
Amber Goulding <gouldinga1@cardiff.ac.uk>
2026-08-23 17:54:17 +0100

    Add full datagen
    bench: 440670

--- datagen/cli.py
+++ datagen/cli.py
@@ -1,16 +1,36 @@
 import argparse
+from datetime import datetime
+from pathlib import Path
+import shutil
 
 from .game import GameRunner
 
-def main () -> int:
+def main() -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--count", type=int)
+    parser.add_argument("-c", "--count", type=int, required=True)
+    parser.add_argument("-o", "--out", type=Path)
+
     args = parser.parse_args()
 
-    game_count = args.count
+    if args.out is not None:
+        fout = args.out
+    else:
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        fout = Path.home() / ".surveyor-data" / f"data-{timestamp}.surveyor-data"
 
-    with GameRunner(1) as game_runner:
-        for i in range(game_count):
-            game_runner.generate_game()
+    fout.parent.mkdir(parents=True, exist_ok=True)
+
+    temp = fout.with_suffix(f".1.surveyor-data")
+
+    with temp.open("w") as output:
+        with GameRunner(output) as game_runner:
+            for _ in range(args.count):
+                game_runner.generate_game()
+
+    with fout.open("ab") as output:
+        with temp.open("rb") as input:
+            shutil.copyfileobj(input, output)
+
+    temp.unlink()
 
     return 0
--- datagen/game.py
+++ datagen/game.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import random
+from typing import TextIO
 
 import chess
 import chess.engine
@@ -22,56 +23,29 @@ class _Game:
     def __init__(self, fen: str):
         self._fen = fen
         self._moves = []
+        self._winner = None
 
     def push_move(self, move: str, score: int, is_mate: bool = False) -> None:
         self._moves.append(_Move(move, score))
 
+    def set_outcome(self, winner: chess.Color | None):
+        self._winner = winner
+
     def stringify(self) -> str:
-        return f"{self._fen} {' '.join(move.stringify() for move in self._moves)}"
+        return f"{self._winner_string()} {self._fen} {' '.join(move.stringify() for move in self._moves)}"
+
+    def _winner_string(self) -> str:
+        if self._winner == chess.WHITE:
+            return "1.0"
+
+        if self._winner == chess.BLACK:
+            return "0.0"
+
+        return "0.5"
 
 class GameRunner:
-    def __init__(self, id: int):
-        self._id = id
-        self._engine = chess.engine.SimpleEngine.popen_uci("./surveyor")
-
-        self._engine.configure({
-            "Hash": HASH,
-            "SoftNodes": True,
-        })
-from dataclasses import dataclass
-import random
-
-import chess
-import chess.engine
-
-from .config import NODES, HASH, VERIFICATION_SEARCH_NODES, VERIFICATION_BOUND
-
-@dataclass(frozen=True)
-class _Move:
-    move: str
-    score: int
-    is_mate: bool = False
-
-    def stringify(self) -> str:
-        if self.is_mate:
-            return f"{self.move} m{self.score}"
-        else:
-            return f"{self.move} {self.score}"
-
-class _Game:
-    def __init__(self, fen: str):
-        self._fen = fen
-        self._moves = []
-
-    def push_move(self, move: str, score: int, is_mate: bool = False) -> None:
-        self._moves.append(_Move(move, score))
-
-    def stringify(self) -> str:
-        return f"{self._fen} {' '.join(move.stringify() for move in self._moves)}"
-
-class GameRunner:
-    def __init__(self, id: int):
-        self._id = id
+    def __init__(self, output: TextIO):
+        self._writer = output
         self._engine = chess.engine.SimpleEngine.popen_uci("./surveyor")
 
         self._engine.configure({
@@ -100,7 +74,12 @@ class GameRunner:
 
             board.push(move)
 
-        print(game.stringify())
+        outcome = board.outcome()
+
+        if outcome is not None:
+            game.set_outcome(outcome.winner)
+
+        print(game.stringify(), file=self._writer)
         
 
     def _generate_board(self) -> chess.Board:
@@ -125,45 +104,4 @@ class GameRunner:
 
     def __exit__(self, exc_type, exc_value, traceback):
         self._engine.quit()
-
-    def generate_game(self):
-        board = self._generate_board()
-        game = _Game(board.fen())
-
-        while not board.is_game_over():
-            result = self._engine.play(
-                board,
-                chess.engine.Limit(nodes=NODES),
-                info=chess.engine.INFO_SCORE,
-            )
-
-            move = result.move
-            score = result.info["score"].relative
-
-            if score.is_mate():
-                game.push_move(move.uci(), score.mate(), True)
-            else:
-                game.push_move(move.uci(), score.score())
-
-            board.push(move)
-
-        print(game.stringify())
-        
-
-    def _generate_board(self) -> chess.Board:
-        while True:
-            board = chess.Board()
-
-            for _ in range(random.randint(5, 8)):
-                board.push(random.choice(list(board.legal_moves)))
-
-            info = self._engine.analyse(
-                board,
-                chess.engine.Limit(nodes=VERIFICATION_SEARCH_NODES),
-            )
-
-            score = info["score"]
-
-            if -VERIFICATION_BOUND < score.relative < VERIFICATION_BOUND:
-                return board
             
