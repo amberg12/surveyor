1de096b34839329a835099771024af04275df663
amberg12 <amber.g.dev@protonmail.com>
2026-08-29 02:41:22 +0100

    Introduce 1m Soft Node Positions (#108)
    bench: 3406437

--- datagen/game.py
+++ datagen/game.py
@@ -50,7 +50,7 @@ class GameRunner:
 
         self._engine.configure({
             "Hash": HASH,
-            "SoftNodes": False,
+            "SoftNodes": True,
         })
 
     def generate_game(self):
